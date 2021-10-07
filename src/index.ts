101d89845ac9d4d99924365bbd22dfc8c2b20407
Lucas Rissi <rissi469@gmail.com>
2021-10-07 20:12:45 -0300

    chore: add typeorm, config database

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,28 @@
+import "reflect-metadata";
+import { createConnection } from "typeorm";
 import express from "express";
-const app = express();
+import bodyParser from "body-parser";
+import helmet from "helmet";
+import cors from "cors";
 
-app.get("/", (req, res) => {
-  res.send("Executando... \n Express + Node Typescript Server");
-});
+//Connects to the Database -> then starts the express
+createConnection()
+  .then(async (connection) => {
+    // Create a new express application instance
+    const app = express();
 
-app.listen(4000, () => {
-  console.log(`[server]: Servidor está executando em http://localhost:4000`);
-});
+    // Call midlewares
+    app.use(cors());
+    app.use(helmet());
+    app.use(bodyParser.json());
+
+    //Set all routes from routes folder
+    app.use("/", (req, res) => {
+      res.send("ok");
+    });
+
+    app.listen(4000, () => {
+      console.log("Servidor iniciado na porta 4000!");
+    });
+  })
+  .catch((error) => console.log(error));
